e6baee4d24229ecd2dafe174692d193c4591b62f
Anti-Particle <63258253+Anti-Particle@users.noreply.github.com>
2022-01-29 11:17:43 -0500

    Update forceCalculations.py
    Added angle calculation

--- forceCalculations.py
+++ forceCalculations.py
@@ -1,19 +1,28 @@
 import math
 
 def shooterCalculations(distanceFromTopHub):
-  hubStackHeight = 109/8 #Feet
-  LimelightSensorHeight = 13/3 #Feet
-
+#   hubStackHeight = 109/8 #Feet
+#   LimelightSensorHeight = 13/3 #Feet
+  
+  hubHeight = 2.64 #meters
+  limeLightHeight = 1.321 #meters
+  shooterHeight = 0 #meters
+  distanceFromTopHub = (distanceFromTopHub/3.281) + 0.1 #converts feet to meters and determines the shooters distance from the 
+  AoA = math.radians(-20)
   # Get Limelight Data
   # Convert to Feet
 
-  hubStackHeight = hubStackHeight-LimelightSensorHeight
-  distanceFromHubBase = math.sqrt(math.pow(distanceFromTopHub,2)-math.pow(hubStackHeight,2))
+  # hubStackHeight = hubStackHeight-LimelightSensorHeight
+  
+  distanceFromHubBase = math.sqrt(math.pow(distanceFromTopHub,2)-math.pow((hubHeight-limeLightHeight),2))
   rawShooterAngle = math.asin((hubStackHeight)/(distanceFromTopHub))
+  
+  shootingAngle = math.degrees(math.atan(((distanceFromHubBase * math.tan(AoA)) - (2 * (hubHeight-limeLightHeight)))/(-1 * distanceFromHubBase)))
+  shootingSpeed = 0 
 
   # Add physics and calculus here. Update angle. Get speed
 
-  print("Shooter Angle: "+str(rawShooterAngle))
+  print("Shooter Angle: "+str(shootingAngle))
   print("Distance from Hubs: "+str(distanceFromHubBase))
   
   return (rawShooterAngle, distanceFromHubBase)
