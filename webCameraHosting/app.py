b7b30ae8eff1f0d724241c10686763e0a6b67581
Spirit-Supreme <68172138+Spirit-Supreme@users.noreply.github.com>
2022-02-27 18:07:05 -0500

    Added multistream + it works

--- webCameraHosting/app.py
+++ webCameraHosting/app.py
@@ -1,9 +1,8 @@
-#ifconfig: get ipv4 address
-#run using flask run -h (ipv4 address) -p 5802
-
-from flask import Flask, render_template, Response
+# import the necessary packages
 import numpy as np
 import cv2
+import time
+from flask import Flask, render_template, Response
 
 from netifaces import interfaces, ifaddresses, AF_INET
 for ifaceName in interfaces():
@@ -13,12 +12,78 @@ for ifaceName in interfaces():
             local_ip = ' '.join(addresses)
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)  # use 0 for web camera #'cams' for all connects cams
+
+import argparse
+parser = argparse.ArgumentParser(description='Choose Color')
+parser.add_argument("-c", help="Pick Ball Color")
+args = parser.parse_args()
+
+# define HoughCircles constants
+ROUNDNESS_THRESH = 10
+CENTER_DETECT_THRESH = 60
+MIN_RADIUS = 20
+# construct the argument parse and parse the arguments
+vs = cv2.VideoCapture(0)
+# define the lower and upper boundaries of the blue or red
+# ball in the HSV color space, then initialize the
+# list of tracked points
+blueLower = (95, 90, 20)
+blueUpper = (135, 255, 255)
+red1Lower = (165, 90, 20)
+red1Upper = (180, 255, 255)
+red2Lower = (0, 90, 20)
+red2Upper = (15, 255, 255)
+# allow the camera or video file to warm up
+time.sleep(2.0)
+
+def Mask():
+    while True:
+        # Capture frame-by-frame
+        success, frame = vs.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            # grab the current frame
+            _, frame = vs.read()
+            # resize the frame, blur it, and convert it to the HSV
+
+            blurred = cv2.GaussianBlur(frame, (101, 101), 0)
+            hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+            # construct a mask for red or blue, then perform a series of dilations and erosions to remove any small blobs
+            # left in the mask
+            if(args.c.lower() == "blue"):
+                mask = cv2.inRange(hsv, blueLower, blueUpper)
+            elif(args.c.lower() == "red"):
+                mask = cv2.inRange(hsv, red1Lower, red1Upper) + cv2.inRange(hsv, red2Lower, red2Upper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            # use Hough Circle Transform to find the roundest object on the screen and trace its perimeter
+            circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, 2, 50, param1=ROUNDNESS_THRESH,
+                                    param2=CENTER_DETECT_THRESH, minRadius=MIN_RADIUS, maxRadius=0)
+            if circles is not None:
+                circles = np.uint16(np.around(circles))
+                biggest_circle = circles[[i[0][2] for i in circles].index(max([i[0][2] for i in circles]))]
+                center = (biggest_circle[0][0], biggest_circle[0][1])
+                # circle center
+                cv2.circle(mask, center, 1, (255, 0, 255), 3)
+                # circle outline
+                radius = biggest_circle[0][2]
+                print((center[0] - 640) / 8000)
+
+                cv2.circle(mask, center, radius, (255, 0, 255), 3)
+                cv2.circle(frame, center, 1, (255, 0, 0), 3)
+                cv2.circle(frame, center, radius, (0, 0, 255), 3)
+            else:
+                print(None)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 def raw_gen():  # Raw Video Feed
     while True:
         # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
+        success, frame = vs.read()  # read the camera frame
         if not success:
             break
         else:
@@ -27,69 +92,21 @@ def raw_gen():  # Raw Video Feed
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-def blue_gen():  # Blue Video Feed
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-
-            b, g, r = cv2.split(frame)
-            zeros_ch = np.zeros(frame.shape[0:2], dtype="uint8")
-            frame = cv2.merge([b, zeros_ch, zeros_ch])
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-def red_gen():  # Red Video Feed
-    while True:
-        # Capture frame-by-frame
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-
-            b, g, r = cv2.split(frame)
-            zeros_ch = np.zeros(frame.shape[0:2], dtype="uint8")
-            frame = cv2.merge([zeros_ch, zeros_ch, r])
-
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/test_feed')
-def test_screen():
-    return("hello world")
-
+@app.route('/mask') #Base root
+def mask_feed():
+    return Response(Mask(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/raw_feed')
 def raw_feed():
     #Video streaming route. Put this in the src attribute of an img tag
     return Response(raw_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/masks/blue')
-def blue_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(blue_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/masks/red')
-def red_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(red_gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
 @app.route('/') #Base root
 def index():
     """Video streaming home page."""
     return render_template('root.html')
 
-
+    
 if __name__ == '__main__':
     #app.run(debug=True)
     app.run(host=local_ip, debug=True,port="5800",use_reloader=False)
