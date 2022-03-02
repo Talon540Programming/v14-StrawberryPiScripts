9d063e00975e111341ed0efca865766a7c94a06f
Spirit-Supreme <68172138+Spirit-Supreme@users.noreply.github.com>
2022-03-02 16:54:50 -0500

    Update Setup.sh

--- Scripts/Setup.sh
+++ Scripts/Setup.sh
@@ -29,6 +29,3 @@ sudo apt install python3-pip git curl dpkg debian-archive-keyring
 echo Installing required pip modules
 #Install NetworkTables, OpenCV2 and its dependencies
 /usr/bin/pip3 install opencv-contrib-python-headless numpy pynetworktables flask netifaces
-
-
-
